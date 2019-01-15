c9d4eff46f7edbd8fca0c60f18f8b5bca952a167
jivinjules <juliehodges05@gmail.com>
2019-01-15 13:50:56 -0600

    heroku update

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const routes = require('./routes')
-require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,10 +9,11 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
+// Serve up static assets (usually on her
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-
+  require('dotenv').config()
   app.get('/', (request, response) => {
     response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   })
@@ -29,6 +29,6 @@ app.use(routes)
 // require('./routes/api/lightning')(app);
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
